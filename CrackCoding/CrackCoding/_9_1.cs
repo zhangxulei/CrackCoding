d23ea460a51967a60e59f70ab0f668d72d3959be
zhangxulei <zxl04@sohu.com>
2015-01-26 22:26:11 +0800

    CrackCoding9.3

---
+++ CrackCoding/CrackCoding/_9_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CrackCoding
+{
+	public class _9_1
+	{
+		public _9_1 ()
+		{
+		}
+
+		public static int WaysCount (int n)
+		{
+			if (n < 0) {
+				return 0;
+			}
+
+			if (n == 0) {
+				return 1;
+			}
+
+			return WaysCount (n - 1) + WaysCount (n - 2) + WaysCount (n - 3);
+		}
+
+		public static int WaysCount (int n, int[] map)
+		{
+			if (n < 0) {
+				return 0;
+			}
+
+			if (n == 0) {
+				return 1;
+			}
+
+			if (map [n] > 0) {
+				return map [n];
+			}
+
+			map [n] = WaysCount (n - 1, map) + WaysCount (n - 2, map) + WaysCount (n - 3, map);
+
+			return map [n];
+		}
+	}
+}
+
---
+++ CrackCoding/CrackCoding/_9_3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CrackCoding
+{
+	public class _9_3
+	{
+		public _9_3 ()
+		{
+		}
+
+		public int FindMagicIndex (int[] array, int startIndex, int endIndex)
+		{
+			if (startIndex < 0 || endIndex >= array.Length || startIndex > endIndex) {
+				return -1;
+			}
+
+			int midIndex = (startIndex + endIndex) / 2;
+
+			int mid = array [midIndex];
+
+			if (mid == midIndex) {
+				return midIndex;
+			}
+
+			int leftEnd = Math.Min (midIndex - 1, mid);
+			int leftIndex = FindMagicIndex (array, startIndex, leftEnd);
+
+			if (leftIndex >= 0) {
+				return leftIndex;
+			}
+
+			int rightStart = Math.Max (midIndex + 1, mid);
+			int rightIndex = FindMagicIndex (array, rightStart, endIndex);
+
+			if (rightIndex >= 0) {
+				return rightIndex;
+			}
+		}
+	}
+}
+
